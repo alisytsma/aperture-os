170df7ac3ad71e12382fb37dd8b8f66ba00bd48a
alisytsma <alissa.sytsma1@marist.edu>
2018-11-03 19:46:04 -0400

    Programs in queue run properly now

--- source/host/memoryAccessor.ts
+++ source/host/memoryAccessor.ts
@@ -13,11 +13,11 @@ module TSOS {
     export class MemoryAccessor {
 
         public static readMemory(position: number): string {
-            return _Memory.memArray[_Memory.memArraySegment][position];
+            return _Memory.memArray[_CPU.program.segment][position];
         }
 
         public static writeMemory(position: number,  val: string): void {
-            console.log("Current val: " + _Memory.memArray[position] + ", pos: " + position + ", updated val: " + val);
+            console.log("Segment: " + _CPU.program.segment + "Current val: " + _Memory.memArray[_CPU.program.segment][position] + ", pos: " + position + ", updated val: " + val);
             val = (+val).toString(16).toUpperCase();
             if(val.length == 1)
                 _Memory.memArray[_CPU.program.segment][position] = "0" + val;
