f3aea419d569a810dbdd3738c96514a69cab4af9
alisytsma <alissa.sytsma1@marist.edu>
2018-12-15 18:42:13 -0500

    Round robin scheduling working

--- source/os/scheduler.ts
+++ source/os/scheduler.ts
@@ -24,6 +24,15 @@ module TSOS {
             //get the current running program
             if(_Kernel.runningQueue.length > 1) {
 
+                // check to see if any of the memory is free without swapping
+                if(_Memory.mem0Free || _Memory.mem1Free || _Memory.mem2Free && _Kernel.pcbDiskList.length > 0) {
+                    // if track 2 is the one with data, otherwise track 3
+                    if (!TSOS.FileSystemDeviceDriver.checkDisk(2))
+                        TSOS.FileSystemDeviceDriver.rollOut(2, 2);
+                    else
+                        TSOS.FileSystemDeviceDriver.rollOut(3, 2);
+                }
+
                 //if cycle count greater than quantum
                 if (this.cycleCount >= _CPU.quantum) {
                     this.runCount++;
@@ -42,51 +51,68 @@ module TSOS {
                         //set current program status as ready
                         if(_CPU.program != null)
                             _CPU.program.status = "Ready";
-                        else
+                        else  //move to first program
                             _CPU.program = _Kernel.readyQueue[0];
-                        //move to first program
+
+                        // if there's a program on the disk
                         if (_Kernel.pcbDiskList.length > 0) {
 
                             var memSegment;
 
-                            if (_Kernel.readyQueue[this.programToSwap].segment == 0) {
-                                _Memory.mem0Free = true;
-                                memSegment = 0;
+                            // check to see if any of the memory is free without swapping
+                            if(_Memory.mem0Free || _Memory.mem1Free || _Memory.mem2Free){
+                                // if track 2 is the one with data, otherwise track 3
+                                if(!TSOS.FileSystemDeviceDriver.checkDisk(2))
+                                    TSOS.FileSystemDeviceDriver.rollOut(2, 2);
+                                else
+                                    TSOS.FileSystemDeviceDriver.rollOut(3, 2);
+                            } else { // if it's not we must swap
+                                // set mem segment to free of the program we're swapping
+                                if (_Kernel.readyQueue[this.programToSwap].segment == 0) {
+                                    _Memory.mem0Free = true;
+                                    memSegment = 0;
+                                }
+                                else if (_Kernel.readyQueue[this.programToSwap].segment == 1) {
+                                    _Memory.mem1Free = true;
+                                    memSegment = 1;
+                                }
+                                else if (_Kernel.readyQueue[this.programToSwap].segment == 2) {
+                                    _Memory.mem2Free = true;
+                                    memSegment = 2;
+                                }
+
+                                // set the program we're swapping's segment to 99 and add it to the disk array
+                                _Kernel.readyQueue[this.programToSwap].segment = 99;
+                                _Kernel.pcbDiskList.push(_Kernel.readyQueue[this.programToSwap]);
+
+                                // if track 2 is open, roll into track 2 and roll out from track 3
+                                if (TSOS.FileSystemDeviceDriver.checkDisk(2)) {
+                                    TSOS.FileSystemDeviceDriver.rollIn(_Memory.memArray[memSegment].toString(), 2);
+                                    TSOS.FileSystemDeviceDriver.rollOut(3, this.programToSwap);
+
+                                    // else if track 3 is open, roll into track 3 and roll out from track 2
+                                } else if (TSOS.FileSystemDeviceDriver.checkDisk(3)) {
+                                    TSOS.FileSystemDeviceDriver.rollIn(_Memory.memArray[memSegment].toString(), 3);
+                                    TSOS.FileSystemDeviceDriver.rollOut(2, this.programToSwap);
+                                }
+
+                                // set program to the one we just swapped in
+                                _CPU.program = _Kernel.readyQueue[this.programToSwap];
+
+
+                                // incriment program to swap as long as it's under 2, otherwise go to 0
+                                this.programToSwap++;
+                                if (this.programToSwap > 2)
+                                    this.programToSwap = 0;
+
                             }
-                            else if (_Kernel.readyQueue[this.programToSwap].segment == 1) {
-                                _Memory.mem1Free = true;
-                                memSegment = 1;
-                            }
-                            else if (_Kernel.readyQueue[this.programToSwap].segment == 2) {
-                                _Memory.mem2Free = true;
-                                memSegment = 2;
-                            }
-
-                            _Kernel.readyQueue[this.programToSwap].segment = 99;
-
-                            _Kernel.pcbDiskList.push(_Kernel.readyQueue[this.programToSwap]);
-
-                            if(TSOS.FileSystemDeviceDriver.checkDisk(2)){
-                                TSOS.FileSystemDeviceDriver.rollIn(_Memory.memArray[memSegment].toString(),2);
-                                TSOS.FileSystemDeviceDriver.rollOut(3, this.programToSwap);
-
-                            } else if(TSOS.FileSystemDeviceDriver.checkDisk(3)) {
-                                console.log("Roll " + _Kernel.pcbDiskList[1].processId + " into track 3");
-                                TSOS.FileSystemDeviceDriver.rollIn(_Memory.memArray[memSegment].toString(), 3);
-                                TSOS.FileSystemDeviceDriver.rollOut(2, this.programToSwap);
-                            }
-
-                            _CPU.program = _Kernel.readyQueue[this.programToSwap];
-
-
-                            this.programToSwap++;
-                            if(this.programToSwap > 2)
-                                this.programToSwap = 0;
-
                         } else {
-
-                        _CPU.runningPID = _Kernel.runningQueue[0].processId;
-                        _CPU.program = _Kernel.readyQueue[_CPU.runningPID];
+                            //sort the running queue by processId
+                            _Kernel.readyQueue.sort(function(a, b){
+                                return a.processId-b.processId
+                            });
+                            _CPU.runningPID = 0;
+                            _CPU.program = _Kernel.readyQueue[_CPU.runningPID];
                         }
                         //context switch interrupt
                         if (_CPU.program != null)
